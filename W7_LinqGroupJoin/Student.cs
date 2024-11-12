5d325e69d6cc54ace75adfeeba042301a24b3091
AenuHub <jesuskane92@gmail.com>
2024-11-12 12:26:57 +0300

    create Student.cs

---
+++ W7_LinqGroupJoin/Student.cs
@@ -0,0 +1,15 @@
+﻿namespace W7_LinqGroupJoin;
+
+public class Student
+{
+    public int StudentId { get; set; }
+    public string StudentName { get; set; }
+    public int ClassId { get; set; }
+
+    public Student(int studentId, string studentName, int classId)
+    {
+        StudentId = studentId;
+        StudentName = studentName;
+        ClassId = classId;
+    }
+}
