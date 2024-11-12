673b59448060d08c0495c79fedcaf6edc5495c3e
AenuHub <jesuskane92@gmail.com>
2024-11-12 12:28:13 +0300

    create Class.cs

---
+++ W7_LinqGroupJoin/Class.cs
@@ -0,0 +1,13 @@
+﻿namespace W7_LinqGroupJoin;
+
+public class Class
+{
+    public int ClassId { get; set; }
+    public string ClassName { get; set; }
+
+    public Class(int classId, string className)
+    {
+        ClassId = classId;
+        ClassName = className;
+    }
+}
